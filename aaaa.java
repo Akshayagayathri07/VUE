9c99f2578abf19cc3f77e786b9e74668f13210ed
Akshayagayathri07 <akshayagayathri7@gmail.com>
2020-04-26 16:29:55 +0530

    new file

---
+++ aaaa.java
@@ -0,0 +1,14 @@
+public class aaaa
+{
+static void main(String args[])
+{
+try{
+int a=0;
+int b=25/a;
+}
+catch(ArrayIndexOutOfBoundsException e){}
+catch(ArithmeticException e){System.out.println("ggj");}
+catch(Exception e){System.out.println("j");}
+
+}
+}
